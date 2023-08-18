6a86a874303ad756e8f27689bde856c1c11355e1
Joseph Adogeri <jadogeri@uno.edu>
2023-08-18 07:43:53 -0500

    added hook useResults for api requests

---
+++ src/hooks/useResults.js
@@ -0,0 +1,36 @@
+import yelp from "../api/yelp"
+import { useState, useEffect } from "react"
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: "new orleans"
+                }
+            }
+            )
+            setResults(response.data.businesses)
+            setErrorMessage('')
+        } catch (e) {
+            console.log('something went wrong terminal')
+
+            setErrorMessage('something went wrong\nerror message : ' + e.message)
+        }
+
+    }
+
+    useEffect(() => {
+        searchApi('pizza')
+    }, [])
+
+
+    return [searchApi, results, errorMessage];
+
+};
+
--- src/screens/SearchScreen/SearchScreen.js
+++ src/screens/SearchScreen/SearchScreen.js
@@ -1,40 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native"
 import SearchBar from "../../components/SearchBar/SearchBar"
-import yelp from "../../api/yelp"
+import useResults from "../../hooks/useResults";
 
 
 
 const SearchScreen = () => {
 
     const [searchTerm, setSearchTerm] = useState("");
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('')
-
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search')
-            setResults(response.data.businesses)
-            setErrorMessage('')
-        } catch (e) {
-            console.log('something went wrong')
-            console.log('e')
-
-            setErrorMessage('something went wrong\n' +e.message)
-        }
-
-    }
-
-    useEffect(() => {
-        searchApi('chicken')
-    }, [{}])
+    const [searchApi,results,errorMessage] = useResults();
+    
     return <View>
 
         <SearchBar searchTerm={searchTerm}
             onTermChange={(newTerm) => { setSearchTerm(newTerm) }}
             onSearchTermSubmit={() => searchApi(searchTerm)} />
         {errorMessage ? <Text>{errorMessage} </Text> : null}
-        <Text>the number of items in array are {results.toString()}</Text>
+        <Text>the number of items in array are {results.length}</Text>
 
     </View>
 }
